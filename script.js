98979e11b13dd17c3b99405fef6f6ee6b3fb7e09
scuby <c7573167070@gmail.com>
2022-07-27 19:21:11 -0400

    Implementation of loops instead of CSS grid layout for game board

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-// script.js
+// sketchy
 
 const grid = document.getElementById('grid');
 
-function buildSquare(){
-    const square = document.createElement('span');
-    grid.appendChild(square);
+function buildGrid(){
+    let i = 1;
+    let j = 1;
+    while (i <= 16){
+        const row = document.createElement('div');
+        row.classList.add('row' + i);
+        j = 1;
+        while (j <= 16){
+            const column = document.createElement('div');
+            column.classList.add('column' + j);
+            row.appendChild(column);
+            j++;
+        }
+        grid.appendChild(row);
+        i++;
+    }
 }
 
-buildSquare();
+buildGrid();

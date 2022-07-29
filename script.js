c5be2ddcacb8266cb4df61952c666283cee61ccb
scuby <c7573167070@gmail.com>
2022-07-29 09:41:44 -0400

    Random cell color assignment, gradually darken to black on subsequent hovers, and other aesthetic improvements

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // sketchy
 
-const grid = document.querySelector('#grid');
+const btn = document.getElementById("button");
+const showSize = document.createElement('div');
+showSize.classList.add('showSize');
+
+const grid = document.querySelector('#grid')
 let gridSize = 10;
 buildGrid(gridSize);
 
@@ -19,6 +23,8 @@ function buttonPress(){
 }
 
 function buildGrid(gridSize){
+    showSize.textContent = `${gridSize} x ${gridSize}`;
+    btn.parentElement.parentElement.appendChild(showSize);
     let i = 1;
     let j = 1;
     while (i <= gridSize){
@@ -39,10 +45,39 @@ function buildGrid(gridSize){
     const cols = document.querySelectorAll('.column');
     cols.forEach((col) => {
         col.addEventListener("mouseover", () => {
-            col.classList.add('hovered');
+            for (let i = 1; i <= 10; i++){
+                // console.log(col.className);
+            if (!col.classList.contains('hovered')){
+                col.classList.add('hovered');
+                col.classList.add(`s${i}`)
+                const h = getRandomNumber();
+                col.style.backgroundColor = `hsl(${h}, 50%, 80%)`;
+                break;
+            } else if (col.classList.contains('hovered') && 
+                        (col.classList.contains('s10'))){
+                        col.classList.add('blackout');
+                        break;
+            } else if (col.classList.contains('hovered') && 
+                        (!col.classList.contains(`s${i}`))){
+                        col.classList.add(`s${i}`);
+                        break;
+            }
+        }
         });
     });
 }
+        
+function getRandomNumber() {
+    const randomNumber = (maxNum) => {
+        return Math.floor(Math.random() * maxNum);
+    };
+    return randomNumber(360);
+}
 
-const btn = document.getElementById("button");
-btn.addEventListener("click", function(){buttonPress()});
+btn.addEventListener("click", function(){
+    const resetSize = document.querySelector('.showSize');
+    resetSize.parentNode.lastChild.remove();
+    buttonPress()});
+
+showSize.textContent = `${gridSize} x ${gridSize}`;
+btn.parentElement.parentElement.appendChild(showSize);
